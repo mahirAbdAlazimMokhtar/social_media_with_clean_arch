8049e01bac3cb65ddccae703323f9b9d2c3ebe50
Mahir <mahr.bwb@gmail.com>
2023-12-26 19:01:30 +0300

    Post Model Done ✅😍

---
+++ lib/src/shared/data/models/post_model.dart
@@ -0,0 +1,45 @@
+import 'user_model.dart';
+import '../../domain/entities/post.dart';
+
+class PostModel {
+  final String id;
+  final UserModel user;
+  final String caption;
+  final String assetPath;
+
+  PostModel({
+    required this.id,
+    required this.user,
+    required this.caption,
+    required this.assetPath,
+  });
+
+  factory PostModel.fromJson(
+    Map<String, dynamic> post,
+    Map<String, dynamic> user,
+  ) {
+    return PostModel(
+      id: post['id'],
+      user: UserModel.fromJson(user),
+      caption: post['caption'],
+      assetPath: post['assetPath'],
+    );
+  }
+  // factory postModel from Entity
+  factory PostModel.fromEntity(Post post) {
+    return PostModel(
+        id: post.id,
+        user: UserModel.fromEntity(post.user),
+        caption: post.caption,
+        assetPath: post.caption);
+  }
+
+  Post toEntity() {
+    return Post(
+      id: id,
+      user: user.toEntity(),
+      caption: caption,
+      assetPath: assetPath,
+    );
+  }
+}
