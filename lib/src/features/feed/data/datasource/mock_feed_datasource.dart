139e7c62917c3465a1550e554f9cfc0ddc1430c7
Mahir <mahr.bwb@gmail.com>
2024-01-01 14:07:45 +0300

    finish Mock feed data source

---
+++ lib/src/features/feed/data/datasource/mock_feed_datasource.dart
@@ -0,0 +1,34 @@
+import 'package:insta/src/shared/data/datasource/post_data.dart';
+import 'package:insta/src/shared/data/datasource/user_data.dart';
+import 'package:insta/src/shared/data/models/post_model.dart';
+
+import '../../../../shared/domain/entities/post.dart';
+
+abstract class MockFeedDatasource {
+  Future<List<Post>> getPosts();
+  Future<List<Post>> getPostsByUser(String userId);
+}
+
+class MocFeedDatasourceImpl implements MockFeedDatasource {
+  @override
+  Future<List<Post>> getPosts() async {
+    await Future.delayed(const Duration(milliseconds: 200), () {});
+    return posts.map((post) {
+      Map<String, dynamic> user = users.where((user) {
+        return user['id'] == post['userId'];
+      }).first;
+      return PostModel.fromJson(post, user).toEntity();
+    }).toList();
+  }
+
+  @override
+  Future<List<Post>> getPostsByUser(String userId) async {
+    await Future.delayed(const Duration(milliseconds: 200), () {});
+    return posts.where((post) => post['userId'] == userId).map((post) {
+      Map<String, dynamic> user = users.where((user) {
+        return user['id'] == post['userId'];
+      }).first;
+      return PostModel.fromJson(post, user).toEntity();
+    }).toList();
+  }
+}
